e395e714648daa7df2bf02b66d98f9fc40201586
Proton748 <sahilraj@gmail.com>
2026-03-23 11:37:22 +0530

    Fix: Read CORE_ORIGIN from enviorment variables

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const app = express();
 app.use(cors({
   origin: "https://gymflow-frontend.onrender.com",
   credentials: true
+methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],   
+allowedHeaders: ['Content-Type', 'Authorization'],        
+optionsSuccessStatus: 200                                 
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
